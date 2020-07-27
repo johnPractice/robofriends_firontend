32dfa21294e6a94b392cd3d22113539220233ec6
Reza Mousavian <rezamosavian1014@gmail.com>
2020-07-26 18:32:14 -0700

    added roboCard and CardList

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import CardList from "./Components/CardList";
+import {robots} from "./data/robots";
+import SearchBox from "./Components/SearchBox";
+//App class Component
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        //define state for App Component
+        this.state={
+            robots:robots,
+            searchField:''
+        }
+    }
+    //search field event declare
+    //after change to arrow function solve the this refer
+    onSearchChange=(e)=>{
+        const value = e.target.value.toLowerCase();
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        this.setState({...this.state,searchField:value});
+    };
+    render() {
+        //after changing the searchField re-render the DOM and page
+        const filteredRobots = this.state.robots.filter(robot=>{
+            return robot.name.toLowerCase().includes(this.state.searchField);
+        });
+        return (
+            <div className="tc">
+                <h1 className='blue fw9-ns'>Robo Friends</h1>
+                <SearchBox onSearchChange={this.onSearchChange} value={this.state.seagreen}/>
+                <CardList robots={filteredRobots}/>
+
+            </div>
+        );
+    }
 }
-
 export default App;
---
+++ src/Components/SearchBox.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const SearchBox=(props)=>{
+    const {onSearchChange,value} = props
+    return(
+        <div className='pa2'>
+          <input
+              value={value}
+              onChange={onSearchChange}
+              className='pa3 ba b--green hover-bg-lightest-blue'
+              type='search'
+              placeholder='search robots'
+          />
+        </div>
+    );
+};
+export default SearchBox;
