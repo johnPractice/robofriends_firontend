17e1ee98468f6b8569828f10a823db9198ee58c3
Reza Mousavian <rezamosavian1014@gmail.com>
2020-07-27 08:56:45 -0700

    Updates

---
+++ precache-manifest.e6307477b525bb6ec072aafdd89d1e6d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd792b12fa2cb55c99ea20220c209e87",
+    "url": "/robofriends_firontend/index.html"
+  },
+  {
+    "revision": "1854e09de1a134c9655d",
+    "url": "/robofriends_firontend/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "980118ae6a3b6b9caa0b",
+    "url": "/robofriends_firontend/static/css/main.6b349e54.chunk.css"
+  },
+  {
+    "revision": "1854e09de1a134c9655d",
+    "url": "/robofriends_firontend/static/js/2.226587da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends_firontend/static/js/2.226587da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "980118ae6a3b6b9caa0b",
+    "url": "/robofriends_firontend/static/js/main.5ea57c46.chunk.js"
+  },
+  {
+    "revision": "2fc2585b87607ea68387",
+    "url": "/robofriends_firontend/static/js/runtime-main.1f511b29.js"
+  }
+]);
