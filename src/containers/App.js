b3c58730dcc99bdbecded38d920ca6cc70fa56c3
Reza Mousavian <rezamosavian1014@gmail.com>
2020-07-31 00:01:47 -0700

    added redux-thunk

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {setSearchField} from '../actions';
+import {requestRobots, setSearchField} from '../actions';
 import {connect} from 'react-redux';
 
 import CardList from "../Components/CardList";
@@ -9,14 +9,7 @@ import Scroll from "../Components/Scroll";
 import ErrorBoundry from "../Components/ErrorBoundry";
 //App class Component
 class App extends React.Component {
-    constructor(props) {
-        console.log('in constructor');
-        super(props);
-        //define state for App Component
-        this.state={
-            robots:[],
-        }
-    }
+
     //search field event declare
     //after change to arrow function solve the this refer
     // onSearchChange=(e)=>{
@@ -30,26 +23,27 @@ class App extends React.Component {
     }
     //component did mount
     componentDidMount() {
-        console.log('in component did mount');
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(res=>res.json())
-            .then(users=>
-            {
-                console.log(users);
-                this.setState({...this.state,robots:users});
-            })
-            .catch(e=>console.error(e));
+        // console.log('in component did mount');
+        // fetch('https://jsonplaceholder.typicode.com/users')
+        //     .then(res=>res.json())
+        //     .then(users=>
+        //     {
+        //         console.log(users);
+        //         this.setState({...this.state,robots:users});
+        //     })
+        //     .catch(e=>console.error(e));
+        this.props.onRequestRobots()
     }
 
     render() {
         console.log('in render');
-        const {searchField,onSearchChange} = this.props;
+        const {searchField,onSearchChange,robots,isPending} = this.props;
         //after changing the searchField re-render the DOM and page
-        const filteredRobots = this.state.robots.filter(robot=>{
+        const filteredRobots = robots.filter(robot=>{
             return robot.name.toLowerCase().includes(searchField.toLowerCase());
         });
         return (
-        this.state.robots.length === 0?
+        isPending?
             <h1>Loading :)</h1>
        :
             <div className="tc">
@@ -67,13 +61,18 @@ class App extends React.Component {
 //define map state to props
 const mapStateToProps =(state)=>{
     return {
-        searchField: state.searchField
+        searchField: state.searchRobots.searchField,
+        robots: state.requestRobots.robots,
+        isPending: state.requestRobots.isPending,
+        error: state.requestRobots.error,
+
     }
 };
 //define mapDispatchToProps
 const  mapDispatchToProps=(dispatch)=>{
     return{
-        onSearchChange : (e)=>dispatch(setSearchField(e.target.value))
+        onSearchChange : (e)=>dispatch(setSearchField(e.target.value)),
+        onRequestRobots:()=>requestRobots(dispatch)
     }
 };
 export default connect(mapStateToProps,mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'tachyons';
 import {Provider} from 'react-redux';
-import {createStore,applyMiddleware} from "redux";
+import {createStore,applyMiddleware,combineReducers} from "redux";
 import {createLogger} from "redux-logger";
-import {searchRobots } from './reducers';
+import {searchRobots,requestRobotsReducer} from './reducers';
+import thunk from "redux-thunk";
 import App from './containers/App';
 
 const logger = createLogger();
-const store = createStore(searchRobots,applyMiddleware(logger));
+const rootReducer = combineReducers({searchRobots,requestRobots: requestRobotsReducer});
+const store = createStore(rootReducer,applyMiddleware(thunk,logger));
 ReactDOM.render(
   <React.StrictMode>
       <Provider store={store}>
