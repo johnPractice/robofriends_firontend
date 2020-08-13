68e5795dfa100029bd3a61c01d3336b9af841bce
Seyed <rezamosavian1014@gmail.com>
2020-08-12 23:46:19 -0700

    added some feature for pwa app

--- src/Components/SearchBox.js
+++ src/Components/SearchBox.js
@@ -4,7 +4,9 @@ const SearchBox = (props) => {
     const {onSearchChange, value} = props;
     return (
         <div className='pa2'>
+            {/*for accessibility e.x for screen reader and so ....*/}
             <input
+                aria-label='Search Robots'
                 value={value}
                 onChange={onSearchChange}
                 className='pa3 ba b--green hover-bg-lightest-blue'
